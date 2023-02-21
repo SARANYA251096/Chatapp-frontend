49caa644f1adaeb5ae34e6c68811e1a9423a68a0
SARANYA251096 <saranyaayyamuthu96@gmail.com>
2023-02-21 20:26:12 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
         {/* <Header /> */}
         <Routes>
           <Route element={<PublicRoutes />}>
+            <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/chat" element={<JoinChat socket={socket} />} />
