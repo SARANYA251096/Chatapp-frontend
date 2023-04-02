8bead7edb9847220988518a05c56add4b6be46d9
SARANYA251096 <saranyaayyamuthu96@gmail.com>
2023-04-02 18:42:46 +0530

    update

--- src/Hooks/useFindUser.js
+++ src/Hooks/useFindUser.js
@@ -3,34 +3,30 @@ import axios from "axios";
 
 const useFindUser = () => {
   const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
-  
-    
+  const [loading, setLoading] = useState(false);
+
   const getUser = async () => {
     setLoading(true);
     try {
       const response = await axios.get(
         `${process.env.REACT_APP_BASE_URL}/user`,
-         { withCredentials: true }
-        );
-        if (response.data.success) {
-          
-            setUser(response.data.user);
-            setLoading(false);
-        }
-        
-    } catch (error) {
+        { withCredentials: true }
+      );
+      if (response.data.success) {
+        setUser(response.data.user);
         setLoading(false);
-        console.log('Error: ', error);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log("Error: ", error);
     }
   };
-    useEffect(() => {
-        setLoading(false);
-        getUser();
-    }, []);
+  useEffect(() => {
+    setLoading(false);
+    getUser();
+  }, []);
 
-    return [user, setUser, setLoading];
-
+  return [user, setUser, setLoading];
 };
 
 export default useFindUser;
